b83ff438a63b672014a55c4310b794790c8ae0ba
Omair <omair@khan.com>
2018-10-30 17:03:34 +0530

    addded time prog

---
+++ Prog/prog15/datetime.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <math.h>
+
+int cday=30,cmon=10,cyea=2018,chou=26,cmin=29,csec=35;
+
+void check(char arr[30])
+{
+	int i,j=0,k=0,l=0,m=0,n=0,b=0,p;
+	for(i=0,p=3;arr[i]!="/";i++,p--)
+	{
+		j=j+((int)arr[i]-48)*pow(10,p);
+	}
+	for(i=i+1,p=1;arr[i]!="/";i++,p--)
+	{
+		k=k+((int)arr[i]-48)*pow(10,p);
+	}
+	for(i=i+1,p=1;arr[i]!=" ";i++,p--)
+	{
+		l=l+((int)arr[i]-48)*pow(10,p);
+	}
+	for(i=i+1,p=1;arr[i]!=":";i++,p--)
+	{
+		m=m+((int)arr[i]-48)*pow(10,p);
+	}
+	for(i=i+1,p=1;arr[i]!=":";i++,p--)
+	{
+		n=n+((int)arr[i]-48)*pow(10,p);
+	}
+	for(i=i+1,p=1;arr[i]!="\0";i++,p--)
+	{
+		b=b+((int)arr[i]-48)*pow(10,p);
+	}
+	if(b>csec)
+	{
+		cmin-=1;
+		csec+=60;
+	}
+	csec-=b;
+	if(n>cmin)
+	{
+		chou-=1;
+		cmin+=60;
+	}
+	cmin-=n;
+	if(m>chou)
+	{
+		cday-=1;
+		chou+=24;
+	}
+	chou-=m;
+	if(l>cday)
+	{
+		cmon-=1;
+		cday+=30;
+	}
+	cday-=l;
+	if(k>cmon)
+	{
+		cyea-=1;
+		cmon+=12;
+	}
+	cmon-=k;
+	if(j>cyea)
+	{
+		printf("Invalid input\n");
+		return;
+	}
+	cyea-=j;
+
+	printf("%d/%d/%d %d:%d:%d \n",cyea,cmon,cday,chou,cmin,csec);
+
+
+
+}
+
+int main()
+{
+	char arr[30];
+	printf("Enter date and time in the format DD/MM/YYYY HH:MM:SS\n");
+	scanf("%s",&arr);
+	printf("%s\n",arr);
+	check(arr);
+	return 0;
+}
